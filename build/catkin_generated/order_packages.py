aaba29e01967147ffe887bc85b8bd7b674a7ccde
Arezki-Nadji <arezki.m.nadji96@gmail.com>
2021-10-21 16:47:48 +0200

    First nodes

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/arezki/catkin_ws_3/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/arezki/catkin_ws_3/devel;/opt/ros/noetic'.split(';') if '/home/arezki/catkin_ws_3/devel;/opt/ros/noetic' != '' else []
---
+++ build/robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arezki/catkin_ws_3/src/robot
+# Build directory: /home/arezki/catkin_ws_3/build/robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
